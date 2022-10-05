aaa1f6f32ff679692084c68e053b52c6ed0f1273
Omar Omari <94939789+OmarOmari1@users.noreply.github.com>
2022-10-04 22:04:58 -0400

    Authenticator

---
+++ Authenticate.java
@@ -0,0 +1,63 @@
+
+import java.util.Scanner;
+
+    public class Authenticate {
+
+    private int[] actualPassword = {2, 2, 3, 2, 1};
+    private int[] randomNums = new int[10];
+    private int[] enteredDigits = new int[actualPassword.length];
+
+    public void genRandomNum() {
+
+            for (int i = 0; i < 10; i++) {
+                    randomNums[i] = (int)(Math.random() * 3) + 1;
+        }
+    }
+
+    public void printRandomNum() {
+
+        System.out.println("Welcome! To log in, enter the random digits from 1-3 that");
+        System.out.println("correspond to your PIN number.");
+        System.out.println("PIN:  0 1 2 3 4 5 6 7 8 9");
+        System.out.print("NUM:  ");
+
+            for (int i = 0; i < 10; i++) {
+                    System.out.print(randomNums[i] + " ");
+        }
+    }
+
+    public void inputUserEntry () {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("");
+        System.out.println("Enter code: ");
+        String entered = input.next();
+
+            for (int i = 0; i < entered.length(); i++) {
+                enteredDigits[i] = entered.charAt(i) - '0';
+        }
+            if (ValidPassword(actualPassword, enteredDigits, randomNums)) {
+                System.out.println("Correct! You many proceed.");
+        }
+            else {
+                System.out.println("Error, invalid password entered.");
+        }
+    }
+
+
+    private boolean ValidPassword(int[] actualPassword, int[] enteredDigits, int[] randomNums) {
+
+        int Index = 0;
+        boolean Valid = true;
+        while (Valid && (Index < actualPassword.length)) {
+            int Code = actualPassword[Index];
+
+            if (enteredDigits [Index] != randomNums [Code]) {
+                Valid = false;
+        }
+            Index++;
+        }
+        return Valid;
+    }   
+}
